6038938139e45eb31c3cc6c194aae635ca256f32
Colton Marty <cmartydev@gmail.com>
2022-02-18 15:22:05 -0500

    Changed stat getter to associative array

--- model/data-layer.php
+++ model/data-layer.php
@@ -1,6 +1,6 @@
 <?php
 
-function getRaces() {
+/*function getRaces() {
     return array("beastman", "dwarf", "elf", "human", "orc", "vampire");
 }
 
@@ -8,7 +8,30 @@ function getStats(){
     return array("dexterity", "intelligence", "luck", "stamina", "strength");
 }
 
-//need to turn item into a class
 function getItems() {
     return array("key", "skill book", "potion", "magic ring", "coin", "random");
+}*/
+
+/**
+ * Gets all race options in Character Quest.
+ * @return string[] of races
+ */
+function getRaces() {
+    return array("beastman", "dwarf", "elf", "human", "orc", "vampire");
+}
+
+/**
+ * Gets key-value array of all the stat names and their values in Character Quest.
+ * @return string[] of stats
+ */
+function getStats(){
+    return array("dexterity" => "", "intelligence" => "", "luck" => "", "stamina" => "", "strength" => "");
+}
+
+/**
+ * Gets all items in Character Quest.
+ * @return string[] of items
+ */
+function getItems() {
+    return array("sword", "shield", "book", "ring", "pendant", "potion", "skeleton key", "coin");
 }
