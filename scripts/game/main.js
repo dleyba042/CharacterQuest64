b222db808294907b48fa18bc160ddbead876c251
Devin Leyba-Brown <leyba-brown.devin@student.greenriver.edu>
2022-03-01 13:24:58 -0500

    adds item boosts and code to use potion

--- scripts/game/main.js
+++ scripts/game/main.js
@@ -15,6 +15,7 @@ const btnA = document.getElementById('btn-a');
 const btnB = document.getElementById('btn-b');
 const btnC = document.getElementById('btn-c');
 const buttonArr = [btnA,btnB,btnC]; //array of buttons
+const globalStatBoosts = {"intelligence":0,"strength":0,"dexterity":0,"stamina":0,"luck":0}
 
 const armorPath = []; // just fake for testing purposes
 const coinPath = []; // just fake for testing purposes
@@ -37,24 +38,41 @@ const getStats = () =>{
 }
 
 
-//TODO code to add boosts to stats based on items
-//add the rest of the items!!!!!!!
+
 const addBoosts = () =>{
 
     let items = inventoryList.children;
 
     for(let i = 0; i<items.length; i++){
 
+        switch(items[i].id){
 
-        if(items[i].id === "sword"){
-
-            document.getElementById("strength").innerHTML += ("(+3)");
+            case "sword" : document.getElementById("strength").innerHTML += ("(+3)");
+                           globalStatBoosts['strength'] = 3;
+                           break;
+            case "shield" : document.getElementById("stamina").innerHTML += ("(+3)");
+                           globalStatBoosts['stamina'] = 3;
+                            break;
+            case "book" : document.getElementById("intelligence").innerHTML += ("(+3)");
+                           globalStatBoosts['intelligence'] = 3;
+                             break;
+            case "ring" : document.getElementById("dexterity").innerHTML += ("(+3)");
+                           globalStatBoosts['dexterity'] = 3;
+                            break;
+            case "pendant" : document.getElementById("luck").innerHTML += ("(+3)");
+                           globalStatBoosts['luck'] = 3;
+                            break;
         }
+
+
+
+
+
     }
 
 }
 
-const playerStats = getStats(); // get the player stats and store them in a constant dict.
+const playerStats = getStats(); // get the player stats and store them in a constant gloabal dict.
 
 
 
@@ -75,13 +93,10 @@ window.onload = () => {
 const displayScene = (scene) => {
 
 
-   // console.log("player stats = " + playerStats['luck']);
-
-    addBoosts();
+    addBoosts(); // add these based on whats in the character inventory at the beginning of the turn
 
     // use this boolean to determine stat role at the end
-    let statsIncreased = {"intelligence":false,"strength":false,"dexterity":false,"stamina":false,"luck":false};
-    let increase = 0;
+    let quickStatIncrease = {"intelligence":false,"strength":false,"dexterity":false,"stamina":false,"luck":false};
     let usedAlready = false;
 
     if(contBtn.style.display === "block"){ //hide continue before displaying new scene
@@ -100,7 +115,9 @@ const displayScene = (scene) => {
         displayScenarioChoices(buttonArr,sceneChoices,i);
     }
 
+
     let outcomeArr = scene.getOutcomes(); // store array for choices
+
 
     //add event listeners to the buttons depending on their choices
     for(let i = 0; i< outcomeArr.length; i++){ // display the choices
@@ -108,36 +125,35 @@ const displayScene = (scene) => {
         //the listener expects a callback function so I had to pass an anonymous
         //that would call the function that I wrote at the appropriate time
         //addEventListener to each button pass in global var contButton
-         buttonArr[i].addEventListener("click",  () => buttonEvent(outcomeArr,i,contBtn));
+        //i is used to index the outcome array, quick stat increase is passed in to
+        //increase a stat if a potion was used
+         buttonArr[i].addEventListener("click",  () => buttonEvent(outcomeArr,i,contBtn,quickStatIncrease));
 
     }
 
-        console.log(inventoryList);
-
         //add event listeners to inventory list buttons
+        //used already is to keep track of using only one item per turn
+        //quick stat increase is reset at beginning of each turn and only has one turn of effect
         let itemButtons = inventoryList.children;
 
         for (let i = 0; i < itemButtons.length; i++) {
             itemButtons[i].addEventListener("click",
-                () => useItem(itemButtons[i], itemButtons[i].getAttribute("id"), usedAlready,statsIncreased));
+                () => useItem(itemButtons[i], itemButtons[i].getAttribute("id"), usedAlready,quickStatIncrease));
         }
 
 
-    /*
-    itemButtons.forEach((button) => {
-        button.addEventListener("click", () => alert(" the value of this button is " + button.value));
-    })
-
-     */
 }
 
 //TODO write the code to actually do what the item does
 const useItem = (button,str,usedAlready,statsInc) => {
 
+    //only use one item per turn
+    if(usedAlready){
+        return;
+    }
+
     let statsArr = ["intelligence","strength","dexterity","stamina","luck"];
 
-    //used already is only if we only want to use one item
-    //per turn, just sitting there for now
 
    let arr = str.split(':');
    let name = arr[0].toLowerCase();
@@ -157,12 +173,6 @@ const useItem = (button,str,usedAlready,statsInc) => {
                 break;
    }
 
-
-  //  alert("You used a " + name );
-    //actually do the effects down here
-    //do we want to remove just potions and keys from inventory based on quantity
-    //will weapons always stay and be usable each turn?
-    // us can set the boolean to used stat item for true
 
     if(quantity === 1){
 
@@ -195,20 +205,25 @@ const displayScenarioChoices = (display,choices,index) => {
 //it will add an event listener to the submit button depending on the choice made
 // might need to add decision logic for if game ends or something
 //btn is the continue button being passed in to accept the event listener fo the next turn
-const buttonEvent = (outcomes,index,btn) => {
+const buttonEvent = (outcomes,index,btn,quickInc) => {
 
     //TODO check if a stat item was used and pass the appropriate increased value ito this role
 
-
-    let statTested = outcomes.getStatTested(); // the stat being tested in this instance
+    let statTested = outcomes[index].getStatTested(); // the stat being tested in this instance
     let currentStatLevel = playerStats[statTested]; // get level stored in player stats
 
-   // let playerStat = strengthStat.value; //something like that
-   //then if (statsIncreased['playerStat']){
-    // then we will add increase variable to that stat and then pass it into the decision}
+    //add any stat-boosts that are in global array
+    currentStatLevel+= globalStatBoosts[statTested];
 
+    // console.log("statTested = " + statTested + "value = " + currentStatLevel);
 
-    //TODO check if stats are boosted anf add that to the currentStatLevel value
+    // loop thru quick stats array. add 1 if potion used on correct stat
+
+    Object.keys(quickInc).forEach( (key) => {
+    if(quickInc[key] === true && statTested === key){
+        console.log("stat " + key + " increased by " + 1);
+            currentStatLevel++;
+    }});
 
 
     if(outcomes[index].rollVsPlayer(currentStatLevel)){// player is same stat strength in test case
@@ -243,31 +258,6 @@ const buttonEvent = (outcomes,index,btn) => {
 //TODO TO TAKE THEM ON A SPECIFIC PATH DEPENDING ON THE BUTTON
 
 
-
-
-//NOTES BELOW
-
-//we need to be able to store character stats all in one place and retrieve them
-//to keep the body of stats and stats populated. This CAN ACTUALLY BE DONE WITH PHP
-
-
-
-
-
-
-//we need to be able to store character inventory all in one place an retrieve them
-//to keep the body of inventory and stats populated. This CAN ACTUALLY BE DONE WITH PHP
-
-
-
-
-//function to display a scenario,
-
-
-
-
-
-//I need a function to grab a scenario that gets passed to display scenario
 
 
 
